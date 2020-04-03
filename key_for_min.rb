b08626eeaec399c251f6d48f48416f4f9ad74d1e
Malcom Fetus <sora.vista@gmail.com>
2020-04-03 02:25:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-lowest_key = nil # This is the default value
-lowest_value = nil # This is the default value  
+lowest_key = nil # This is the default key
+lowest_value = nil # This is the default value
 
 end
