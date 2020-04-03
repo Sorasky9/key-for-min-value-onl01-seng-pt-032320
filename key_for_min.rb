eaafdf5f5f5adf6ed44031851ca3eb49c02a9781
Malcom Fetus <sora.vista@gmail.com>
2020-04-03 02:27:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 def key_for_min_value(name_hash)
 lowest_key = nil # This is the default key because it is empty
-lowest_value = nil # This is the default value because it is empty 
+lowest_value = nil # This is the default value because it is empty
+
+name_hash.each do |key, value| 
 
 end
