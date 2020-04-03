5573f19c1af95174b9d07b4555994eff72c3b32a
Malcom Fetus <sora.vista@gmail.com>
2020-04-03 02:24:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  high = :blake
+  low = :adam
 
+ name_hash.each do |high, low|
+     name_hash = []
+     puts nil 
+
+end
+end
 end
